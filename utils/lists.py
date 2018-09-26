2bf34b0faad303563432e36998d743f4122f3f8d
Nicholas Ouellette <nicho@nb.sympatico.ca>
2018-09-26 19:35:48 -0300

    Dwight quotes
    Added new quotes

--- utils/lists.py
+++ utils/lists.py
@@ -10,3 +10,30 @@ shellresponse = [
   'Yes', 'No', 'Nothing', 'Try asking again',
   'I don\'t think so', 'Neither', 'Maybe someday'
 ]
+
+dwightquotes = [ 
+
+  'I am fast. To give you a reference point I am somewhere between a snake and a mongoose… And a panther.',
+  'Through concentration, I can raise and lower my cholesterol at will.',
+  'Congratulations on your one cousin. I have seventy, each one better than the last!',
+  'No, I disagree. \“R\” is one of the most menacing of sounds. That\’s why they call it murder not \‘muckduck\’.',
+  'All you need is love. False. The four basic human necessities are air, water, food, and shelter.',
+  'Before I do anything I ask myself, \'Would an idiot do that?\' and if the answer is yes, I do not do that thing.',
+  'How would I describe myself? Three words: hardworking, alpha male, jackhammer, merciless, insatiable.',
+  'I wish I could menstruate. I\'d be more in tune with the moon and the tides.',
+  'Babies are one of my many areas of expertise. Growing up I performed my own circumcision.',
+  'In an ideal world I would have all ten fingers on my left hand and the right one would just be left for punching.',
+  'I grew up on a farm. I have seen animals having sex in every position imaginable. Goat on chicken. Chicken on goat. Couple of chickens doing a goat, couple of pigs watching.',
+  'Powerpoints are the peacocks of the business world; all show, no meat.',
+  'I am faster than 80% of all snakes.','Always the Padawan, never the Jedi.',
+  'My feelings regenerate at twice the speed of a normal man.',
+  'Reject a woman, and she will never let it go. One of the many defects of their kind. Also, weak arms.',
+  'D.W.I.G.H.T – Determined, Worker, Intense, Good worker, Hard worker, Terrific.',
+  'I am not a security threat. And, my middle name is Kurt, not Fart.',
+  'Michael is like Mozart, and I’m like Butch Cassidy. You mess with Mozart and you\’re gonna get bullet in your head, courtesy of Butch Cassidy.',
+  'Trust me, you are going to want to heeeeeat, my words.',
+  'Why are all these people here? There are too many people on this earth. We need a new plague.',
+  'I am better than you have ever been or ever will be.','I overslept. Damn rooster didn’t crow.',
+  'Learn your rules. You better learn your rules. If you don\’t, you\’ll be eaten in your sleep.'
+]
+  
